9ac10c1cb781c0fe4e4567a0aff5c0b4348aa351
Julian Davis <Prof.JMDavis@gmail.com>
2022-05-23 19:30:52 -0400

    UI and Audio additions
    Buttons to replace generic ones. Pressed and Unpressed sprites. Added audio files and audio scripts.

---
+++ Astronoughts/Assets/Audio/Scripts/AudioFile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[System.Serializable]
+
+public class AudioFile
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    public AudioSource source;
+
+    public bool isLooping;
+    public bool playOnAwake;
+}
---
+++ Astronoughts/Assets/Audio/Scripts/Sound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class Sound : MonoBehaviour
+{
+	public string name;      //Store the name of our music/effect
+	public AudioClip clip;  //Store the actual music/effect
+	public AudioMixerGroup[] masterGroup;
+	public AudioMixerGroup soundsGroup;
+	public AudioMixerGroup musicGroup;
+
+	[Range(0f, 1f)]     //limit the volume range
+	public float volume = 0.75f;    //Store our volume
+
+	[Range(0f, 1f)]
+	public float volumeVariance = 1f;
+
+	[Range(1f, 3f)]     //Limit the pitch Range
+	public float pitch = 1f; //set the pitch for our music/effect
+
+	[Range(0f, 1f)]
+	public float pitchVariance = 1f;
+
+	public bool loop = false; //should this sound loop
+
+	[HideInInspector]
+	public AudioSource source;
+
+	public bool isLooping;
+	public bool playOnAwake;
+}
