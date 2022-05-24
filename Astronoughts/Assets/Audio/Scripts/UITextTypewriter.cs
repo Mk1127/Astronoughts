6d924cd5f95e040176a59f999a3b0450b7811faa
Julian Davis <Prof.JMDavis@gmail.com>
2022-05-24 17:10:09 -0400

    Typewriter text effect
    Instructions text set to typing effect.

--- Astronoughts/Assets/Audio/Scripts/UITextTypewriter.cs
+++ Astronoughts/Assets/Audio/Scripts/UITextTypewriter.cs
@@ -4,40 +4,99 @@
 //Section: (2022SU.SGD.289)
 //Instructor: Amber Johnson
 //////////////////////////////////////////////////////
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class UITextTypewriter : MonoBehaviour
 {
-    public string text;
-    private string tempText = "";
-    public float speed = 0.1f;
+    Text txt;
+    TMP_Text textMeshTxt;
+    string writtenTxt;
+    public Button instructions;
+
+    //Serialize the settings
+    //Imitate a screen cursor
+    [SerializeField] string cursor = "";
+    [SerializeField] float typingSpeed = 0.1f;
+    [SerializeField] float delay = 0f;
+    [SerializeField] bool delayCursor = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("TypeWriter");
     }
 
-
-    IEnumerator TypeWriter()
+    public void OnInstructionsClick()
     {
-        for(int i = 0;i <= text.Length;i++)
+        //Get the text component of text field
+        txt = GetComponent<Text>()!;
+        textMeshTxt = GetComponent<TMP_Text>()!;
+
+        //Check to make sure there a value
+        if(txt != null)
         {
-            tempText = text.Substring(0,i);
-            this.GetComponent<Text>().text = tempText;
-            yield return new WaitForSeconds(speed);
+            //Fill variable
+            writtenTxt = txt.text;
+            txt.text = "";
+
+            StartCoroutine("TextTypewriter");
         }
-        StartCoroutine("ResetText");
+        if(textMeshTxt != null)
+        {
+            //Fill variable
+            writtenTxt = textMeshTxt.text;
+            textMeshTxt.text = "";
+
+            StartCoroutine("TextMeshTypewriter");
+        }
     }
 
-    IEnumerator ResetText()
+
+    IEnumerator TextTypewriter()
     {
-        yield return new WaitForSeconds(2);
-        this.GetComponent<Text>().text = "";
-        Debug.Log(tempText);
+        //The text field is a Text -- Check for pause before starting
+        Debug.Log(writtenTxt);
+        txt.text = delayCursor ? cursor : "";
+        yield return new WaitForSeconds(delay);
+
+        foreach(char c in writtenTxt)
+        {
+            if(txt.text.Length > 0)
+            {
+                txt.text = txt.text.Substring(0,txt.text.Length - cursor.Length);
+            }
+            txt.text += c;
+            txt.text += cursor;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    IEnumerator TextMeshTypewriter()
+    {
+        //The text field is a Text Mesh -- Check for pause before starting
+
+        textMeshTxt.text = delayCursor ? cursor : "";
+        yield return new WaitForSeconds(delay);
+
+        foreach(char c in writtenTxt)
+        {
+            if(textMeshTxt.text.Length > 0)
+            {
+                textMeshTxt.text = textMeshTxt.text.Substring(0,textMeshTxt.text.Length - cursor.Length);
+            }
+            textMeshTxt.text += c;
+            textMeshTxt.text += cursor;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        //Check if a cursor is needed
+        if(cursor != "")
+        {
+            textMeshTxt.text = textMeshTxt.text.Substring(0,textMeshTxt.text.Length - cursor.Length);
+        }
     }
 
 }
