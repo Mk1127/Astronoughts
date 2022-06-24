c5ecdeb08a735a7f3f111af03e838e466cc3a1a9
Julian Davis <Prof.JMDavis@gmail.com>
2022-06-23 22:07:35 -0400

    Start of GameManager to hold player and game data.
    next player transform and functions

--- Astronoughts/Assets/Scripts/Game/GameManager.cs
+++ Astronoughts/Assets/Scripts/Game/GameManager.cs
@@ -1,26 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     #region Variables
-    public static GameManager Instance;
+    public static GameManager gameManager;
+    
+    // player statistics
+    public int playerFuel;
+    public int currentFuel;
+    public int crewNumber;
+    public int currentCrew;
+    public int partsToFind;
+    public int currentParts;
+    public int repairsCompleted;
+
+    // inventory statistics
+    public GameObject slot;
+    public GameObject[] slotsToFill;
+    public Image[] collectedParts;
+    public SortedList Parts;
+ 
+    //game statistics
+    public int sceneCounter;
+    public string currentScene;
+    public string shipState;
+
     #endregion
     // Start is called before the first frame update
     void Awake()
     {
-        if(Instance == null)
+        if(gameManager == null)
         {
-            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            gameManager = this;
         }
-        else if(Instance != this)
+        else if(gameManager != this)
         {
             Destroy(gameObject);
             return;
         }
-        //Persist this instance through level change.
-        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        playerFuel = 100;
+        currentFuel = 100;
+        crewNumber = 0;
+        currentCrew = 0;
+        partsToFind = 5;
+        currentParts = 0;
+        repairsCompleted = 0;
+
+        //game statistics
+        sceneCounter = 0;
+        currentScene = "";
+        shipState = "Incapacitated";
     }
 
 }
