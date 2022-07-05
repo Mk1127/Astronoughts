13c6aefe91fa48d03f5ce74dfa80ce081c0055ae
Julian Davis <Prof.JMDavis@gmail.com>
2022-07-05 15:45:10 -0400

    Increasing size and changing position of mute button

--- Astronoughts/Assets/Scripts/Audio/AudioManager.cs
+++ Astronoughts/Assets/Scripts/Audio/AudioManager.cs
@@ -73,14 +73,14 @@ public class AudioManager : MonoBehaviour
         source = gameObject.GetComponent<AudioSource>();
         if(source.mute == false)
         {
-            if(GUI.Button(new Rect(0,0,35,35),muteTexture))
+            if(GUI.Button(new Rect(Screen.width - 150,0,50,50),muteTexture))
             {
                 gameObject.GetComponent<AudioSource>().mute = true;
             }
         }
         else if(source.mute == true)
         {
-            if(GUI.Button(new Rect(0,0,35,35),playTexture))
+            if(GUI.Button(new Rect(Screen.width - 150,0,50,50),playTexture))
             {
                 gameObject.GetComponent<AudioSource>().mute = false;
             }
