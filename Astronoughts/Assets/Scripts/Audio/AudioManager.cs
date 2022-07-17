6e4f2093d8360aabbd4351cc2315f8b634518bc2
Julian Davis <Prof.JMDavis@gmail.com>
2022-07-17 01:49:04 -0400

    New audio play/mute icons
    these have been installed in controls, credits, hub

--- Astronoughts/Assets/Scripts/Audio/AudioManager.cs
+++ Astronoughts/Assets/Scripts/Audio/AudioManager.cs
@@ -73,14 +73,14 @@ public class AudioManager : MonoBehaviour
         source = gameObject.GetComponent<AudioSource>();
         if(source.mute == false)
         {
-            if(GUI.Button(new Rect(Screen.width - 50,0,50,50),muteTexture))
+            if(GUI.Button(new Rect(Screen.width - 100,0,100,100),muteTexture))
             {
                 gameObject.GetComponent<AudioSource>().mute = true;
             }
         }
         else if(source.mute == true)
         {
-            if(GUI.Button(new Rect(Screen.width - 50,0,50,50),playTexture))
+            if(GUI.Button(new Rect(Screen.width - 100,0,100,100),playTexture))
             {
                 gameObject.GetComponent<AudioSource>().mute = false;
             }
