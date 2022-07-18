b1f6d7dd382dfc42751db6893e2d66df990c9245
Julian Davis <Prof.JMDavis@gmail.com>
2022-07-18 17:39:30 -0400

    Pulled changes

--- Astronoughts/Assets/Scripts/Player/Player_Movement.cs
+++ Astronoughts/Assets/Scripts/Player/Player_Movement.cs
@@ -150,6 +150,12 @@ public class Player_Movement : MonoBehaviour
                 if(playerIS.isHidden)
                 {
                     speed = startSpeed * .75f;
+                    isSprinting = false;
+                }
+                else
+                {
+                    speed = startSpeed;
+                    isSprinting = false;
                 }
             }
 
@@ -161,6 +167,11 @@ public class Player_Movement : MonoBehaviour
                     isSprinting = false;
                 }
             }
+        }
+
+        if(!isGrounded)
+        {
+            speed = startSpeed * .75f;
         }
 
         if (isGrabbing)
@@ -194,20 +205,15 @@ public class Player_Movement : MonoBehaviour
         {
             if (isGrounded)
             {
-                if (reverseGravity)
+                if (!reverseGravity)
                 {
-                    velocity.y = 5f; //If the player is not hovering, on the ground, and gravity is reversed set the Y velocity to 5. Lifts the player;
-                }
-                else
-                {
-                    velocity.y = -1f; //If the player is not hovering, on the ground, and gravity is not reversed set the Y velocity to -0.5. Lowers the player;
+                    velocity.y = -1f; //If the player is not hovering, on the ground, and gravity is reversed set the Y velocity to 5. Lifts the player;
                 }
             }
             else
             {
                 velocity.y -= grav * Time.deltaTime; // If the player is not hovering and is not on the ground set Y velocity by gravity * time, Lowers the player.
             }
-
             velocity.y = Mathf.Clamp(velocity.y, -10, 50);
         }
         else
@@ -225,14 +231,12 @@ public class Player_Movement : MonoBehaviour
         {
             if (!hovering)
             {
-                if (isGrounded)
+                if (thrustGauge > 0)
                 {
-                    if (thrustGauge > 0)
-                    {
-                        hovering = true;
-                        StartCoroutine(StartHover());
-                    }
+                    hovering = true;
+                    StartCoroutine(StartHover());
                 }
+                
             }
         }
         else
@@ -240,11 +244,6 @@ public class Player_Movement : MonoBehaviour
             hovering = false;
             reverseGravity = false;
         }
-
-        /*if(Input.GetKeyUp(KeyCode.Space))
-        {
-
-        }*/
     }
 
     public void Jump()
@@ -272,8 +271,7 @@ public class Player_Movement : MonoBehaviour
 
             if (!hovering)
             {
-                StartCoroutine(RefillThrusters());
-                yield break;
+                break;
             }
         }
 
@@ -285,23 +283,48 @@ public class Player_Movement : MonoBehaviour
     IEnumerator RefillThrusters()
     {
         speed = startSpeed;
+        float timer;
         if (thrustGauge == 0)
         {
-            yield return new WaitForSeconds(3.0f);
+            timer = 2f;
+        }
+        else
+        {
+            timer = 1f;
         }
 
-        while (thrustGauge < 100)
+        while (timer > 0)
         {
-            thrustGauge += Time.deltaTime * 20;
-            thrustGauge = Mathf.Clamp(thrustGauge, 0, 100);
-            yield return null;
+            if(isGrounded)
+            {
+                timer -= Time.deltaTime;
+            }
 
             if (hovering)
             {
+                hovering = true;
                 StartCoroutine(DrainThrusters());
                 yield break;
             }
 
+            yield return null;
+        }
+
+        while (thrustGauge < 100)
+        {
+            if(isGrounded)
+            {
+                thrustGauge += Time.deltaTime * 20;
+                thrustGauge = Mathf.Clamp(thrustGauge, 0, 100);
+            }
+            yield return null;
+
+            if (hovering)
+            {
+                hovering = true;
+                StartCoroutine(DrainThrusters());
+                yield break;
+            }
         }
     }
 
@@ -315,10 +338,13 @@ public class Player_Movement : MonoBehaviour
         StartCoroutine(DrainThrusters());
         StartCoroutine(SparksFade());
 
-        while (transform.position.y < startY + 1)
+        if(isGrounded)
         {
-            velocity.y = 3f;
-            yield return null;
+            while (transform.position.y < startY + 1)
+            {
+                velocity.y = 3f;
+                yield return null;
+            }
         }
 
         reverseGravity = false;
